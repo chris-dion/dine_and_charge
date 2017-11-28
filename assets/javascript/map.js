1cf8e9e00514f5ad6ecdba2fadccc36dfc5c2d14
akayne <15270231+arielkayne@users.noreply.github.com>
2017-11-28 14:13:13 -0600

    added example json and some comments

--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -2,6 +2,7 @@
 var lat = 30.287738;
 var long = -97.729001;
 // API doesn't need a key, current settings: max results=10, distance searched=10, units=miles, export to JSON
+	//JSON export file seems to output in order of distance from lat long
 var queryURL = "https://api.openchargemap.io/v2/poi/?output=json&countrycode=US&maxresults=10&latitude="+lat+"&longitude="+long+"&distance=10&distanceunit=Miles&maxresults=10";
 
 
